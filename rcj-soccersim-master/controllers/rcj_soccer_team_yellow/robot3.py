809359aacf30c680f91feef193ea5798c0abbc4d
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-12-17 17:28:05 +0330

    asdf

--- rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot3.py
+++ rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot3.py
@@ -5,11 +5,21 @@ class MyRobot3(RCJSoccerRobot):
     def run(self):
         self.xb = 0
         self.yb = 0
+        self.xr = 0
+        self.yr = 0
         self.is_ball = True
+        step = 1
         while self.robot.step(TIME_STEP) != -1:
             if self.is_new_data():
                 utils.readData(self)
                 if self.is_ball:
-                    utils.move(self, self.xb, self.yb)
+                    if step == 1:
+                        utils.move(self, self.xb, self.yb-0.1)
+                        if abs(self.xr - self.xb) < 0.01 and abs(self.yr - (self.yb-0.1)) < 0.01:
+                            step = 2
+                    else: 
+                        utils.move(self, self.xb, self.yb)
+                        if abs(self.xr - self.xb) > 0.2 and abs(self.yr - self.yb) > 0.2:
+                            step = 1
                 else:
                     utils.move(self, 0, -0.6)
--- rcj-soccersim-master/controllers/rcj_soccer_team_yellow/utils.py
+++ rcj-soccersim-master/controllers/rcj_soccer_team_yellow/utils.py
@@ -6,10 +6,11 @@ def move(robot,xp,yp):
     a=math.degrees(math.atan2(robot_pos[0]-xp,yp-robot_pos[1]))
     e=heading-a
     m=math.sqrt((robot_pos[0]-xp)**2+(robot_pos[1]-yp)**2)
-    if m<0.05:
-        robot.right_motor.setVelocity(0)
-        robot.left_motor.setVelocity(0)
-    elif e<-10:
+    # if m<0.05:
+    #     robot.right_motor.setVelocity(0)
+    #     robot.left_motor.setVelocity(0)
+    # el
+    if e<-10:
         robot.right_motor.setVelocity(-10)
         robot.left_motor.setVelocity(10)
     elif e>10:
@@ -22,12 +23,14 @@ def move(robot,xp,yp):
 def readData(robot):
     robot_pos = robot.get_gps_coordinates()
     heading = math.degrees(robot.get_compass_heading())
+    robot.xr = robot_pos[0]
+    robot.yr = robot_pos[1]
     if robot.is_new_ball_data():
         ball_data = robot.get_new_ball_data()
         ball_angle = math.degrees(math.atan2(ball_data['direction'][1], ball_data['direction'][0]))
         ball_distance = abs(0.0166666666/abs(ball_data['direction'][2])/math.sqrt(1-ball_data['direction'][2]**2))
-        robot.xb = -math.sin(math.radians(ball_angle + heading)) * ball_distance + robot_pos[0]
-        robot.yb =  math.cos(math.radians(ball_angle + heading)) * ball_distance + robot_pos[1]
+        robot.xb = -math.sin(math.radians(ball_angle + heading)) * ball_distance + robot.xr
+        robot.yb =  math.cos(math.radians(ball_angle + heading)) * ball_distance + robot.yr
         robot.is_ball = True
     else:
         robot.is_ball = False
