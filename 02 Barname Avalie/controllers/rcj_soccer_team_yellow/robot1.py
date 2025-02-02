6515233546d30fbee710a8fa6e52197ea9420afa
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-02-02 19:30:04 +0330

    barname nemoone

---
+++ 02 Barname Avalie/controllers/rcj_soccer_team_yellow/robot1.py
@@ -0,0 +1,10 @@
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+from math import *
+from utils import *
+
+class MyRobot1(RCJSoccerRobot):
+    def run(self):
+        while self.robot.step(TIME_STEP) != -1:
+            if self.is_new_data():
+                readData(self)
+                
